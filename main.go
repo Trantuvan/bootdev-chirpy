f45b138a370b85e03c0357d63785de2c0be181a4
trantuvan <trantuvan.kan@gmail.com>
2024-11-24 20:28:51 +0700

    bootdev server static file

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	const port string = "8080"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
-	})
-	server := http.Server{Addr: fmt.Sprintf(":%s", port), Handler: mux}
+	const filepathRoot = "."
+	const port = "8080"
 
-	log.Printf("Servering on port: %s\n", port)
-	log.Fatalf("Server failed: %s", server.ListenAndServe())
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
