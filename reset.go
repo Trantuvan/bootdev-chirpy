219279a2a59e7a8168c7eb41c7a691eb8c3ea928
trantuvan <trantuvan.kan@gmail.com>
2024-12-03 17:15:53 +0700

    remove single quote

--- reset.go
+++ reset.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("'Content-Type'", "'text/plain; charset=utf8'")
+	w.Header().Add("Content-Type", "text/plain; charset=utf8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
 	w.Write([]byte("Hits reset to 0"))
