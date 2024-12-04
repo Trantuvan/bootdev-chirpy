bce010f332a4b20699a51d0cb894165a4d1872e7
trantuvan <trantuvan.kan@gmail.com>
2024-12-04 13:55:34 +0700

    remove extra response.WriteHeader from middleware

--- metrics.go
+++ metrics.go
@@ -22,7 +22,6 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		w.Header().Add("Cache-Control", "no-cache")
-		w.WriteHeader(http.StatusOK)
 		next.ServeHTTP(w, r)
 	})
 }
