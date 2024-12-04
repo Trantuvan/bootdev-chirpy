167201d384611d9cacdf5292917f8a9d52451004
trantuvan <trantuvan.kan@gmail.com>
2024-12-04 15:37:39 +0700

    handle error by log std.out in helpers.httpJson

--- helpers/httpJson.go
+++ helpers/httpJson.go
@@ -2,23 +2,37 @@ package helpers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-func ResponseWithJson(w http.ResponseWriter, statusCode int, payload interface{}) error {
+type errResponse struct {
+	Error string `json:"error"`
+}
+
+func ResponseWithJson(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 	response, err := json.Marshal(payload)
 
 	if err != nil {
-		return err
+		log.Printf("Error marshalling JSON: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(statusCode)
 	w.Write(response)
-	return nil
 }
 
-func ResponseWithError(w http.ResponseWriter, statusCode int, msg string) error {
-	return ResponseWithJson(w, statusCode, map[string]string{"error": msg})
+func ResponseWithError(w http.ResponseWriter, statusCode int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	if statusCode > 499 {
+		log.Printf("Response with 5XX error: %s", msg)
+	}
+
+	ResponseWithJson(w, statusCode, errResponse{msg})
 }
--- validate_chirp.go
+++ validate_chirp.go
@@ -8,6 +8,7 @@ import (
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	const maxChirpLength = 140
 	params := struct {
 		Body string `json:"body"`
 	}{}
@@ -19,12 +20,12 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&params); err != nil {
-		helpers.ResponseWithError(w, http.StatusInternalServerError, "Validate Chirp: cannot parse json")
+		helpers.ResponseWithError(w, http.StatusInternalServerError, "Validate Chirp: cannot parse json", err)
 		return
 	}
 
-	if len(params.Body) > 140 {
-		helpers.ResponseWithError(w, http.StatusBadRequest, "Validate Chirp: Chirp is too long")
+	if len(params.Body) > maxChirpLength {
+		helpers.ResponseWithError(w, http.StatusBadRequest, "Validate Chirp: Chirp is too long", nil)
 		return
 	}
 
